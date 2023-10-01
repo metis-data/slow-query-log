fcb362054f8a962074197d0ad87839e3440d0f75
Issackr <issackr@metisdata.io>
2023-10-01 17:07:43 +0300

    fix query log 0

--- lib/slow-query-log.ts
+++ lib/slow-query-log.ts
@@ -154,7 +154,10 @@ export class MetisSqlCollector {
       .map((log) => {
         try {
           const { log_time: logTime, database_name: dbName, virtual_transaction_id: transactionId, message } = log;
-          const queryId = log.query_id ?? this.getQueryIdFromTransaction(transactionId, [...bindLogs, ...parseLogs]);
+          const queryId =
+            log.query_id !== '0'
+              ? log.query_id
+              : this.getQueryIdFromTransaction(transactionId, [...bindLogs, ...parseLogs]);
           const [durationString, ...planObj] = message.split('plan:');
           const parsed = JSON.parse(planObj.join('plan:').trim());
           const { ['Query Text']: query, ...plan } = parsed;
